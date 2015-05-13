87f903ec20b0d71b31cd0ea707461e0b845f197b
eddieluis37 <eddieluis37@users.noreply.github.com>
2015-05-12 19:41:19 -0500

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('luis', 'HomeController@index');
+Route::get('gabriela', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
